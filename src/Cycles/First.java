b5170f50c9e3162d2851abfbfc5364277a874284
DenysChupryna <anytechdocs@gmail.com>
2016-12-13 23:10:32 +0200

    Сделал 1ю

--- src/Cycles/First.java
+++ src/Cycles/First.java
@@ -1,5 +1,22 @@
 package Cycles;
 
 
-public class First {
+import java.util.Scanner;
+
+class First {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number");
+        String number = scanner.next();
+        int numberInt = Integer.parseInt(number);
+        int sum = 0;
+        while (numberInt > 0){
+            sum += numberInt % 10;
+            numberInt /= 10;
+
+        }
+        System.out.println(sum);
+
+
+    }
 }
